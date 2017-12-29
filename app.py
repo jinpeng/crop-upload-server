3df69786441b28dec82d6bd3dad1d24ff58354a8
dongjp <dongjp@unipus.cn>
2017-12-29 08:53:56 +0800

    add config file

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import configparser
 import falcon
 import uptoken
 
-access_key = 'NUbGljTaPJ42x7Py1_s7MY2kVrAvgpHnTICdl5_p'
-secret_key = 'zhwRBRcvhVWsjwyO3QK_J1U60QMj5TjO_KPzdNXD'
-bucket_name = 'jitu'
+config = configparser.ConfigParser()
+config.read('config.ini')
+access_key = config['qiniu']['access_key']
+secret_key = config['qiniu']['secret_key']
+bucket_name = config['qiniu']['bucket_name']
 
 api = application = falcon.API()
 uptoken = uptoken.UpToken(access_key, secret_key, bucket_name)
 api.add_route('/uptoken', uptoken)
-
